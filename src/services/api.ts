d3430c114ebcb465c8d68c334d48a58b5f15a505
Lucas Carvalho <decarvalh@gmail.com>
2023-03-13 22:04:46 -0300

    fix api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,25 +1,17 @@
 import axios from "axios";
+import { scopes } from "./scopes";
 
-axios.defaults.baseURL = "https://www.googleapis.com/youtube/v3";
-
+const baseUrl = "https://www.googleapis.com/youtube/v3";
+const OAuthGoogle = "https://accounts.google.com/o/oauth2/auth";
+const GoogleRegister = "https://accounts.google.com/signup";
 const host = import.meta.env.VITE_HOST;
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 const redirectUri = import.meta.env.VITE_GOOGLE_REDIRECT_URI;
 const key = import.meta.env.VITE_GOOGLE_API_KEY;
 
-const scopes = [
-  "https://www.googleapis.com/auth/youtube",
-  "https://www.googleapis.com/auth/youtube.channel-memberships.creator",
-  "https://www.googleapis.com/auth/youtube.force-ssl",
-  "https://www.googleapis.com/auth/youtube.readonly",
-  "https://www.googleapis.com/auth/youtube.upload",
-  "https://www.googleapis.com/auth/youtubepartner",
-  "https://www.googleapis.com/auth/youtubepartner-channel-audit",
-];
+axios.defaults.baseURL = baseUrl;
 
 export function getOAuthUrl() {
-  const url = "https://accounts.google.com/o/oauth2/auth";
-
   const params = {
     client_id: clientId,
     redirect_uri: host + redirectUri,
@@ -28,14 +20,12 @@ export function getOAuthUrl() {
     include_granted_scopes: "true",
     state: "pass-through value",
   };
-
   const query = new URLSearchParams(params).toString();
-
-  return `${url}?${query}`;
+  return `${OAuthGoogle}?${query}`;
 }
 
 export function getRegisterUrl() {
-  return "https://accounts.google.com/signup";
+  return GoogleRegister;
 }
 
 export function setBearerToken(token?: string) {
---
+++ src/services/scopes.ts
@@ -0,0 +1,9 @@
+export const scopes = [
+  "https://www.googleapis.com/auth/youtube",
+  "https://www.googleapis.com/auth/youtube.channel-memberships.creator",
+  "https://www.googleapis.com/auth/youtube.force-ssl",
+  "https://www.googleapis.com/auth/youtube.readonly",
+  "https://www.googleapis.com/auth/youtube.upload",
+  "https://www.googleapis.com/auth/youtubepartner",
+  "https://www.googleapis.com/auth/youtubepartner-channel-audit",
+];
