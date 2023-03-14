44b3bf4b78146db25c974707ef6a7e69dbf6ab8d
Lucas Carvalho <decarvalh@gmail.com>
2023-03-13 21:43:12 -0300

    fix

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -6,6 +6,6 @@ export function Skeleton({ className = "h-3" }: SkeletonProps) {
   return (
     <div
       className={`animate-pulse bg-neutral-700 rounded-sm mb-1 w-full ${className}`}
-    ></div>
+    />
   );
 }
